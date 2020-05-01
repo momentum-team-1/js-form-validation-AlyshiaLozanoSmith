78a5a7c626cd40b0703308b1dddc28f799a9054f
Alyshia Lozano Smith <alyshiapolk@gmail.com>
2020-05-01 17:50:29 -0400

    figured out the helper functions, added them to validation functions

--- script.js
+++ script.js
@@ -15,93 +15,92 @@ form.addEventListener ('submit', function (event) {
     validateCC()
     validateCVV()
     validateExpiration()
+    removeErrorMessage()
+      
 
 })
 
 //helper functions
-function errormsg(){
+
+function makeValid(parentElement){
+    parentElement.classList.add('input-valid')
+    parentElement.classList.remove('input-invalid')
+
+}
+
+ function makeInvalid(parentElement){
+    parentElement.classList.add('input-invalid')
+    parentElement.classList.remove('input-valid')
+}
+
+function errormsg(fieldID){
     let errordiv = document.createElement('div')
-        let errormsg = document.createTextNode('This field is required')
-        errordiv.appendChild(errormsg)
-        document.getElementById('name-field').appendChild(errordiv)
-
-        
-
+    let errormsg = document.createTextNode('This field is required')
+    errordiv.appendChild(errormsg)
+    document.getElementById(fieldID).appendChild(errordiv)
 }
 
 //validating functions
 function validateName (){
     let nameInput = document.querySelector('#name')
     let nameValue = nameInput.value 
-    let parentEl = nameInput.parentElement 
+    let nameParent = nameInput.parentElement 
 
     if(nameValue){
         console.log('name is valid')
-        parentEl.classList.add('input-valid')
-        parentEl.classList.remove('input-invalid')
+        makeValid(nameParent)
 
     }else{
         console.log('name is not valid')
-        parentEl.classList.add('input-invalid')
-        parentEl.classList.remove('input-valid')
-        errormsg()
+       makeInvalid(nameParent)
+       errormsg('name-field')
     }
-
-
 }
 
 function validateCarYear (){
     let carYearInput = document.querySelector('#car-year')
     let carYearValue = carYearInput.value 
-    let parentEl = carYearInput.parentElement 
+    let yearParent = carYearInput.parentElement 
 
-    if(carYearValue){
+    if(carYearValue > 1900 ){
         console.log('year is valid')
-        parentEl.classList.add('input-valid')
-        parentEl.classList.remove('input-invalid')
+       makeValid(yearParent)
 
     }else{
         console.log('year is not valid')
-        parentEl.classList.add('input-invalid')
-        parentEl.classList.remove('input-valid')
-        errormsg()
+       makeInvalid(yearParent)
+        errormsg('car-field')
     }
-
-
 }
 
 function validateCarMake (){
     let carMakeInput = document.querySelector('#car-make')
     let carMakeValue = carMakeInput.value 
-    let parentEl = carMakeInput.parentElement 
+    let makeparent = carMakeInput.parentElement 
 
     if(carMakeValue){
         console.log('make is valid')
-        parentEl.classList.add('input-valid')
-        parentEl.classList.remove('input-invalid')
+        makeValid(makeparent)
 
     }else{
         console.log('make is not valid')
-        parentEl.classList.add('input-invalid')
-        parentEl.classList.remove('input-valid')
-        errormsg()
+       makeInvalid(makeparent)
+        errormsg('car-field')
     }
 }
 
     function validateCarModel() {
         let carModelInput = document.querySelector('#car-model')
         let carModelValue = carModelInput.value
-        let parentEl = carModelInput.parentElement
+        let modelparent = carModelInput.parentElement
 
         if (carModelValue){
             console.log('model is valid')
-            parentEl.classList.add('input-valid')
-            parentEl.classList.remove('input-invalid')
+            makeValid(modelparent)
         } else {
             console.log('model is invalid')
-            parentEl.classList.remove('input-valid')
-            parentEl.classList.add('input-invalid')
-            errormsg()
+            makeInvalid(modelparent)
+            errormsg('car-field')
         }
           
     }
@@ -110,89 +109,80 @@ function validateCarMake (){
     function validateStartDate (){
         let startDateInput = document.querySelector('#start-date')
         let startDateValue = startDateInput.value 
-        let parentEl = startDateInput.parentElement 
+        let startdateparent = startDateInput.parentElement 
     
         if(startDateValue){
             console.log('date is valid')
-            parentEl.classList.add('input-valid')
-            parentEl.classList.remove('input-invalid')
+           makeValid(startdateparent)
     
         }else{
             console.log('date is not valid')
-            parentEl.classList.add('input-invalid')
-            parentEl.classList.remove('input-valid')
-            errormsg()
+            makeInvalid(startdateparent)
+            errormsg('start-date-field')
         }
 }
 
 function validateNumOfDays(){
     let numOfDaysinput = document.querySelector('#days')
     let numOfDaysValue = numOfDaysinput.value
-    let parentEl = numOfDaysinput.parentElement
+    let numOfDaysParent = numOfDaysinput.parentElement
 
     if(numOfDaysValue){
         console.log('number of days is valid')
-        parentEl.classList.add('input-valid')
-        parentEl.classList.remove('input-invalid')
+        makeValid(numOfDaysParent)
 
     }else{
         console.log('number of days is not valid')
-        parentEl.classList.add('input-invalid')
-        parentEl.classList.remove('input-valid')
-        errormsg()
+        makeInvalid(numOfDaysParent)
+        errormsg('days-field')
     }
 }
 
 function validateCC(){
     let CCinput = document.querySelector('#credit-card')
     let CCValue = CCinput.value
-    let parentEl = CCinput.parentElement
+    let CCParent = CCinput.parentElement
 
     if(CCValue){
         console.log('credit card number of days is valid')
-        parentEl.classList.add('input-valid')
-        parentEl.classList.remove('input-invalid')
+        makeValid(CCParent)
 
     }else{
         console.log('credit card number is not valid')
-        parentEl.classList.add('input-invalid')
-        parentEl.classList.remove('input-valid')
-        errormsg()
+        makeInvalid(CCParent)
+        errormsg('credit-card-field')
     }
 }
 
 function validateCVV(){
     let cvvinput = document.querySelector('#cvv')
     let cvvValue = cvvinput.value
-    let parentEl = cvvinput.parentElement
+    let cvvParent= cvvinput.parentElement
 
     if(cvvValue){
         console.log('CVV is valid')
-        parentEl.classList.add('input-valid')
-        parentEl.classList.remove('input-invalid')
+        makeValid(cvvParent)
 
     }else{
         console.log('CVV is not valid')
-        parentEl.classList.add('input-invalid')
-        parentEl.classList.remove('input-valid')
-        errormsg()
+       makeInvalid(cvvParent)
+        errormsg("cvv-field")
     }
 }
+
 
 function validateExpiration (){
     let expirationinput = document.querySelector('#expiration')
     let expirationValue = expirationinput.value
-    let parentEl = expirationinput.parentElement
+    let expirationparent = expirationinput.parentElement
 
     if(expirationValue){
         console.log('expiration date is valid')
-        parentEl.classList.add('input-valid')
-        parentEl.classList.remove('input-invalid')
+        makeValid(expirationparent)
 
     }else{
         console.log('expiration date is not valid')
-        parentEl.classList.add('input-invalid')
-        parentEl.classList.remove('input-valid')
-        errormsg()
+        makeInvalid(expirationparent)
+        errormsg('expiration-field')
     }
 }
